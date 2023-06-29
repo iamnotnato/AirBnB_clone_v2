2be161fc2e34f3a65409f5f72770298fd7e5d0af
Alan Nato <alanwnato@gmail.com>
2023-06-29 16:44:32 +0300

    v7

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,112 +1,89 @@
 #!/usr/bin/python3
-"""This is the file storage class for AirBnB"""
-import json
-import os
+"""Storage engine for databases"""
 
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm import scoped_session
-from sqlalchemy import (create_engine)
-
-from models.base_model import BaseModel, Base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.base_model import Base
+from os import getenv
 from models.user import User
 from models.state import State
 from models.city import City
-from models.amenity import Amenity
 from models.place import Place
 from models.review import Review
+from models.amenity import Amenity
+
+HBNB_MYSQL_USER = getenv("HBNB_MYSQL_USER")
+HBNB_MYSQL_PWD = getenv("HBNB_MYSQL_PWD")
+HBNB_MYSQL_HOST = getenv("HBNB_MYSQL_HOST")
+HBNB_MYSQL_DB = getenv("HBNB_MYSQL_DB")
+
+classes = {"User": User, "State": State, "City": City,
+           "Place": Place, "Review": Review, "Amenity": Amenity}
 
 
 class DBStorage:
-    """This class serializes instances to a JSON file and
-    deserializes JSON file to instances
-    Attributes:
-        __file_path: path to the JSON file
-        __objects: objects will be stored
     """
-    __classes = [State, City, User, Place, Review, Amenity]
+    Database storage representation
+    """
     __engine = None
     __session = None
-    # SETUP RELATIONSHIP WITH City => State TODO
 
     def __init__(self):
-        """init function for dbstorage to initialize engine and
-            session with sqlalchemy
-        """
-        try:
-            # build connection string
-            user = os.environ.get('HBNB_MYSQL_USER')
-            password = os.environ.get('HBNB_MYSQL_PWD')
-            host = os.environ.get('HBNB_MYSQL_HOST')
-            db = os.environ.get('HBNB_MYSQL_DB')
-            env = os.environ.get('HBNB_ENV')
-            mandatory = [user, password, host, db]
-            # verify we got all neccessary attributes
-            for attr in mandatory:
-                if attr is None:
-                    print("Missing mandatory env var")
+        """Initialize class instance"""
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.format(
+            HBNB_MYSQL_USER,
+            HBNB_MYSQL_PWD,
+            HBNB_MYSQL_HOST,
+            HBNB_MYSQL_DB), pool_pre_ping=True)
 
-            conn_str = "mysql+mysqldb://{}:{}@{}/{}".format(
-                        user, password, host, db)
-            # create engine and session object with connection string
-            self.__engine = create_engine(conn_str, pool_pre_ping=True)
-
-            # drop all tables in DB if test env
-            if env == 'test':
-                Base.metadata.drop_all(bind=self.__engine, checkfirst=True)
-        except Exception as E:
-            print("raised exception in init")
-            print(E)
+        env = getenv("HBNB_ENV")
+        if (env == "test"):
+            Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
-        """returns a dictionary
-        Return:
-            returns a dictionary of objects stored in sqlalchemy
-        """
-        results = []
-        if cls is None:
-            for c in self.__classes:
-                for result in self.__session.query(c):
-                    results.append(result)
+        """Query all instances of given class name"""
+
+        instances = {}
+        if cls:
+            for row in self.__session.query(cls).all():
+                key = ("{}.{}".format(cls.__name__, row.id))
+                row.to_dict()
+                instances[key] = row
         else:
-            result = self.__session.query(eval(cls))
-            results = result.all()
-        # return the results as a dictionary with class.id as key
-        return {"{}.{}".format(result.__class__.__name__, result.id): result
-                for result in results}
+            for bnb_class in classes.values():
+                rows = self.__session.query(bnb_class).all()
+                for row in rows:
+                    key = ("{}.{}".format(row.__class__.__name__, row.id))
+                    row.to_dict()
+                    instances[key] = row
+        return instances
 
     def new(self, obj):
-        """sets __object to given obj
-        Args:
-            obj: given object
-        """
-        if obj and self.__session:
+        """add obj to current session"""
+        if obj:
             self.__session.add(obj)
 
     def save(self):
-        """serialize the file path to JSON file path
-        """
-        if self.__session:
-            self.__session.commit()
+        """Commmit all changes to current session"""
+        self.__session.commit()
 
     def delete(self, obj=None):
-        """deletes an object from __objects if inside
-        """
-        try:
+        """Delete obj from session"""
+        if obj is not None:
             self.__session.delete(obj)
-        except:
-            pass
 
     def reload(self):
-        """serialize the file path to JSON file path
+        """Create all tables in DB"""
+        Base.metadata.create_all(self.__engine)
+        Session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Scope = scoped_session(Session)
+        self.__session = Scope()
         """
-        try:
-            Base.metadata.create_all(self.__engine)
-            session_factory = sessionmaker(bind=self.__engine,
-                                           expire_on_commit=False)
-            self.__session = scoped_session(session_factory)
-        except Exception as E:
-            print(E)
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine,
+                                       expire_on_commit=False)
+        self.__session = scoped_session(session_factory)()"""
 
     def close(self):
-        """removes our session"""
-        self.__session.remove()
+        """Close current session"""
+        self.__session.close()
--- models/state.py
+++ models/state.py
@@ -1,29 +1,31 @@
 #!/usr/bin/python3
-"""This is the state class"""
-import models
-from models.city import City
+""" State Module for HBNB project """
 from models.base_model import BaseModel, Base
+import models
 from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
-from os import environ
+from os import getenv
+
+storage_type = getenv("HBNB_TYPE_STORAGE")
 
 
 class State(BaseModel, Base):
-    """This is the class for State
-    Attributes:
-        name: input name
-    """
+    """ State class """
     __tablename__ = "states"
-    name = Column(String(128), nullable=False)
+    if (storage_type == 'db'):
+        __tablename__ = "states"
+        name = Column(String(128), nullable=False)
+        cities = relationship('City', backref='state',
+                              cascade='all, delete, delete-orphan')
+    else:
+        name = ""
 
-    cities = relationship("City",
-                          backref="state",
-                          cascade="all, delete-orphan",
-                          passive_deletes=True)
-
-    
-    if environ.get('HBNB_TYPE_STORAGE') != 'db':
         @property
         def cities(self):
-            return [city for city in models.storage.all(
-                City).values() if city.state_id == self.id]
+            """Getter method for all cities"""
+            list_cities = models.storage.all(models.city.City).values()
+            res = []
+            for city in list_cities:
+                if (city.state_id == self.id):
+                    res.append(city)
+            return res
