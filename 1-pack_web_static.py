99db77c112af62cca1933516522428d49211a024
Alan Nato <alanwnato@gmail.com>
2023-07-07 00:22:11 +0300

    pls

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-""" Function that compress a folder """
+"""a script to pack static content into a tarball
+"""
+from fabric.api import *
 from datetime import datetime
-from fabric.api import local
-import os
 
 
 def do_pack():
-    try:
-        if not os.path.exists("versions"):
-            local('mkdir versions')
-        t = datetime.now()
-        f = "%Y%m%d%H%M%S"
-        archive_path = 'versions/web_static_{}.tgz'.format(t.strftime(f))
-        local('tar -cvzf {} web_static'.format(archive_path))
-        return archive_path
-    except:
+    """packages all contents from web_static into .tgz archive
+    """
+    now = datetime.now().strftime("%Y%m%d%H%M%S")
+    local('mkdir -p versions')
+    result = local('tar -czvf versions/web_static_{}.tgz web_static'
+                   .format(now))
+    if result.failed:
         return None
+    else:
+        return result
