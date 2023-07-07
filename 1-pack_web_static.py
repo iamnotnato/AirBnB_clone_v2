50bb6c4cc4d55742efd980433ba749702686e437
Alan Nato <alanwnato@gmail.com>
2023-07-07 23:57:21 +0000

    Try

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -10,11 +10,10 @@ def do_pack():
     """
     now = datetime.now().strftime("%Y%m%d%H%M%S")
     local('mkdir -p versions')
-    result = local('tar -czvf versions/web_static_{}.tgz web_static/'
+    result = local('tar -czvf versions/web_static_{}.tgz web_static'
                    .format(now))
     if result.failed:
         return None
     else:
-        print('web_static packed:')
         return result
         
