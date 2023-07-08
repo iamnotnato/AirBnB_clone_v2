d0cf79e74f5517f142ebc6aec9ddab34cfafdbb8
Alan Nato <alanwnato@gmail.com>
2023-07-08 12:37:03 +0000

    Compress before sending : Added Time Zone

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -4,6 +4,7 @@ import os.path
 from datetime import datetime
 from fabric.api import local
 
+
 def do_pack():
     """Create a tar gzipped archive of the directory web_static."""
     dt = datetime.utcnow()
