3a40408f58605b39b2b99f60ee3b9511ec0693b8
Alan Nato <alanwnato@gmail.com>
2023-08-17 09:27:48 +0300

    fix : Deploy archive!

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -36,22 +36,14 @@ def do_deploy(archive_path):
     ar_name = archive_path[archive_path.find("/") + 1: -4]
     try:
         run('mkdir -p /data/web_static/releases/{}/'.format(ar_name))
-        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
+        run('tar -xzf /tmp/{}.tgz -C /data/web_static/releases/{}/'.format(ar_name, ar_name))
         run('rm /tmp/{}.tgz'.format(ar_name))
-        run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(
-                ar_name, ar_name
-        ))
+        run('mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/'.format(ar_name, ar_name))
         run('rm -rf /data/web_static/releases/{}/web_static'.format(
             ar_name
         ))
         run('rm -rf /data/web_static/current')
-        run('ln -s /data/web_static/releases/{}/ \
-            /data/web_static/current'.format(
-            ar_name
-        ))
+        run('ln -s /data/web_static/releases/{}/ /data/web_static/current'.format(ar_name))
         print("New version deployed!")
         return True
     except:
